84e3d579f7c5fff719a861b4903d6d4a15589474
Sitnitsa Pavel <vip.sitnitsa@mail.ru>
2022-04-29 14:27:20 +0300

    HW3

---
+++ HW3/task1.js
@@ -0,0 +1,7 @@
+let a = "2" + true;
+let b = "2" + 4;
+let c = 2 + true;
+
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ HW3/task4.js
@@ -0,0 +1,7 @@
+let a = String(2);
+let b = +"2";
+let c = !!5;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
